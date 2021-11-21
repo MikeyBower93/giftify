a736ee782a9e55a5accafa88c2da92660e71060f
Michael Bower <mikey.bower93@gmail.com>
2021-11-21 11:44:47 +0000

    force SSL incorrect

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ import Config
 # which you should run after static files are built and
 # before starting your production server.
 config :giftify, GiftifyWeb.Endpoint,
-  cache_static_manifest: "priv/static/cache_manifest.json".
+  cache_static_manifest: "priv/static/cache_manifest.json",
   force_ssl: [hsts: true]
 
 # Do not print debug messages in production
